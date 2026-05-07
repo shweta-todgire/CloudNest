5b8901489f17ecc88cb9d2eec4289a35549abfa8
shweta-todgire <shwetatodgire23@gmail.com>
2026-05-07 09:45:24 -0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -7,11 +7,23 @@ const nextConfig = {
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups", 
+            value: "same-origin-allow-popups",
           },
           {
             key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none", 
+            value: "unsafe-none",
+          },
+
+          // ✅ ADD THIS (CSP FIX)
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              font-src 'self' *.vercel.com *.gstatic.com vercel.live https://*.vercel-storage.com;
+              img-src 'self' data: blob: lh3.googleusercontent.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+            `.replace(/\s{2,}/g, " ").trim(),
           },
         ],
       },
