ed344424f2464f4c18800b2040ac67b494b0b3eb
shweta-todgire <shwetatodgire23@gmail.com>
2026-05-11 08:14:27 -0700

    Final CSP fix

--- next.config.js
+++ next.config.js
@@ -32,57 +32,16 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
 
-            value: `
-              default-src 'self';
-
-              script-src
-                'self'
-                'unsafe-inline'
-                'unsafe-eval'
-                https://apis.google.com
-                https://accounts.google.com
-                https://www.gstatic.com
-                https://www.googleapis.com;
-
-              style-src
-                'self'
-                'unsafe-inline'
-                https://fonts.googleapis.com;
-
-              font-src
-                'self'
-                https://fonts.gstatic.com
-                https://*.gstatic.com
-                data:;
-
-              img-src
-                'self'
-                data:
-                blob:
-                https:
-                lh3.googleusercontent.com;
-
-              connect-src
-                'self'
-                https://identitytoolkit.googleapis.com
-                https://securetoken.googleapis.com
-                https://firestore.googleapis.com
-                https://firebase.googleapis.com
-                https://www.googleapis.com
-                https://apis.google.com
-                wss://*.firebaseio.com;
-
-              frame-src
-                'self'
-                https://accounts.google.com
-                https://*.firebaseapp.com;
-
-              object-src 'none';
-
-              base-uri 'self';
-            `
-              .replace(/\s{2,}/g, ' ')
-              .trim(),
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com; " +
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+              "font-src 'self' https://fonts.gstatic.com https://*.gstatic.com data:; " +
+              "img-src 'self' data: blob: https: lh3.googleusercontent.com; " +
+              "connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://www.googleapis.com https://apis.google.com wss://*.firebaseio.com; " +
+              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com; " +
+              "object-src 'none'; " +
+              "base-uri 'self';",
           },
         ],
       },
